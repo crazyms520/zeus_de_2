263e0dfee53d88bdadbb4df9a90119a9081b275b
CHO <bsuysheep927@gmail.com>
2022-07-27 22:41:22 +0800

    0727 lack special-feature web

--- js/public.js
+++ js/public.js
@@ -9,7 +9,6 @@
 		$('.nav-link').removeClass('active');
 		$(this).find('.nav-link').addClass('active');
 		$('#brand-origin').show("slide", { direction: "left" }, 400);
-		// window.scrollTo({ top: 0, behavior: 'smooth' });
 	})
 
 	$('.happy-skin-show').click(function() {
@@ -20,7 +19,6 @@
 		$('.nav-link').removeClass('active');
 		$(this).find('.nav-link').addClass('active');
 		$('#happy-skin').show("slide", { direction: "down" }, 10);
-		// window.scrollTo({ top: 0, behavior: 'smooth' });
 	})
 
 	$('.special-feature-show').click(function() {
@@ -31,25 +29,51 @@
 		$('.nav-link').removeClass('active');
 		$(this).find('.nav-link').addClass('active');
 		$('#special-feature').show("slide", { direction: "right" }, 400);
-		// window.scrollTo({ top: 0, behavior: 'smooth' });
 	})
 
 	$('.slick-items').slick({
 		infinite: true,
-		slidesToShow: 2,
-  		slidesToScroll: 2,
-		prevArrow:"<img class='a-left control-c prev slick-prev' src='img/arrow_l.svg'>",
-      	nextArrow:"<img class='a-right control-c next slick-next' src='img/arrow_r.svg'>"
+		slidesToShow: 3,
+  		slidesToScroll: 1,
+		prevArrow:"<img class='slick-prev' src='img/arrow_l.svg'>",
+      	nextArrow:"<img class='slick-next' src='img/arrow_r.svg'>",
+		responsive: [
+			{
+				breakpoint: 769,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			}
+		]
 	});
 	
 	
 	$('.btn_more').click(function() {
+		let content = $(this).parents('.item').find('.modal_content_hover');
+		let color = content.attr('data-ph');
+		let ph = content.find('.modal_ph_hover').text();
+		let title = content.find('.modal_title_hover').text();
+		let effect = content.find('.modal_effect_hover').text();
+		let text = content.find('.modal_text_hover').text();
+		let oldColor = $('.modal_content_mobile').attr('data-ph'); 
+
+		$('.modal_ph_mobile').removeClass(oldColor);
+		$('.modal_ph_mobile').addClass(color);
+		$('.modal_ph_mobile').text(ph);
+		$('.modal_title_mobile').text(title);
+		$('.modal_effect_mobile').removeClass(oldColor);
+		$('.modal_effect_mobile').addClass(color);
+		$('.modal_effect_mobile').text(effect);
+		$('.modal_text_mobile').text(text);
+
+		$('.modal_content_mobile').attr('data-ph', color);
 		$(".modal_mask").show();
-		$('.modal_content').show();
+		$('.modal_content_mobile').show();
 
 		$(".modal_mask").click(function() {
 			$(".modal_mask").hide();
-			$('.modal_content').hide();	
+			$('.modal_content_mobile').hide();	
 		})
 	})
 
